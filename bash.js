4434edda4687d4e73861988fb9610447aed5c3dc
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-10 21:56:43 -0700

    refactored bash.js

--- bash.js
+++ bash.js
@@ -8,16 +8,21 @@ process.stdout.write("prompt > ");
 //The stdin "data" event fires after a user types in a line
 process.stdin.on("data", (data) => {
     const cmd = data.toString().trim(); //remove the newline
-    const splitCmd = cmd.split(" ");
-    if (cmd === "pwd") {
-        pwd();
-    } else if (cmd === "ls") {
-        ls();
-    } else if (splitCmd[0] === "cat") {
-        const fileName = splitCmd[1];
-        cat(fileName);
-    } else {
-        process.stdout.write(`You typed: ${cmd}`);
-        process.stdout.write("\nprompt > ");
+    const command = cmd.split(" ");
+
+    switch (command[0]) {
+        case "pwd":
+            pwd();
+            break;
+        case "ls":
+            ls();
+            break;
+        case "cat":
+            const fileName = command[1];
+            cat(fileName);
+            break;
+        default:
+            process.stdout.write(`You typed: ${command.join(" ")}`);
+            process.stdout.write("\nprompt > ");
     }
 });
